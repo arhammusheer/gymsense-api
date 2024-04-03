0ed818e0f0a5874ddf2a4b10a0f2c41fc08ea452
arhammusheer <arhammusheer@gmail.com>
2024-04-03 11:39:03 -0400

    Added strict same site

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -17,6 +17,7 @@ const userController = {
       res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
       });
 
       res.json({ user, token });

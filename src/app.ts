dc90a6453a169f5c2b2275d9ea5a0fda3572e356
arhammusheer <arhammusheer@gmail.com>
2024-04-08 06:40:34 -0400

    Added localhost cors allowed

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
     exposedHeaders: ["Set-Cookie"],
     credentials: true,
-    origin: /croissant\.one$/,
+    origin: [/croissant\.one$/, /localhost:\d+$/],
   })
 );
 
